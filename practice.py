85e90bc0dbf4d28e73da7d5eadc3c0d214e42140
cameronsepahi <cameronsepahi@gmail.com>
2022-01-02 21:23:36 -0800

    init

---
+++ practice.py
@@ -0,0 +1,14 @@
+#Tuples
+
+# dataStruct = tuple(['cameron', 'dave', 'bob', 2])
+# print(dataStruct[3])
+
+#Functions
+
+# def addNums(num1, num2):
+#     return num1+num2
+
+# print(addNums(1,2))
+
+
+
